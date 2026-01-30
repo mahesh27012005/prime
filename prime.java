06cbba2d05e4e267b30c5352b1daa52d40459213
Mahesh <mahesh272005@gmail.com>
2026-01-30 09:08:05 +0530

    Correct prime number
    Fix prime number checking logic to start loop from 2.

---
+++ prime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class PrimeCheck { public static void main(String[] args) { 
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int n = sc.nextInt();
+    int count = 0;
+    if (n <= 1) {
+        System.out.println("Not a prime number");
+        return;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0)
+            count++;
+    }
+    if (count == 2)
+        System.out.println("Prime number");
+    else
+        System.out.println("Not a prime number");
+}
+}
